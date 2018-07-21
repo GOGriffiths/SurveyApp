6482cbf67e9c80f5ba83d5d49e119014e80e2928
Gareth Griffiths <garethowengriffiths@gmail.com>
2018-07-21 17:31:24 +0100

    add mlab db, and session cookie code

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User'); // delcare mode BEFORE passport tries to use it
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+//wire up middleware - all requests go through
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,8 +1,22 @@
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-//known as strategy called 'google'
+const User = mongoose.model('users'); // load users model out of mongoose
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); //(err, identifying info) // * this is NOT the googleId, but the id asigned to the record by mongo.
+});
+//the googel profile id is only really to make the record, but the record id is what we'll uses as user comes and goes.
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+//known as strategy called 'google', hit everytime we do the google oauth flow
 passport.use(
   new googleStrategy(
     {
@@ -11,10 +25,27 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(accessToken);
-      console.log(refreshToken);
-      console.log(profile);
-      console.log(done);
+      // logInfo(accessToken, refreshToken, profile, done);
+
+      User.findOne({ googleId: profile.id }).then(exisitngUser => {
+        //looks at users collections and find first entry where condition met.
+        //async fucntion called with whatever is found, a user or null
+        if (exisitngUser) {
+          done(null, exisitngUser); //tell passport we're finished
+        }
+        if (!exisitngUser) {
+          new User({ googleId: profile.id }) //create a new modal instance
+            .save() //save it
+            .then(user => done(null, user)); // *user is a new modal intance return once its been to db -
+        }
+      });
     }
   )
 );
+
+const logInfo = (accessToken, refreshToken, profile, done) => {
+  console.log('access token', accessToken);
+  console.log('refresh token', refreshToken);
+  console.log('profile', profile);
+  console.log('done', done);
+};
